5297ee38f9409ceec23a150b3652e7953c674c2d
Ibadat-Afridi <ibadatkhanafridi7@gmail.com>
2025-07-03 11:02:00 +0500

    Home menu or menu driven feature

---
+++ view_home_page.h
@@ -0,0 +1,12 @@
+int view_home();
+//The view_home function will do nothing except printing the print statement if login succeeded 
+int view_home()
+{
+  printf("\n===== Home Menu =====\n");
+  printf("1. Add Product\n");
+  printf("2. Delete Product\n");
+  printf("3. Search Product\n");
+  printf("4. Display All Products\n");
+  printf("5. Logout\n");
+  printf("======================\n");
+}
